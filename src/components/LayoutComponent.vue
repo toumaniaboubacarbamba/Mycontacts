dd6ba625dbe32f5be07fffcfc6155d3843bb437e
toumaniaboubacarbamba <toumani.bamba@epitech.eu>
2025-09-16 10:54:45 +0000

    router add

---
+++ src/components/LayoutComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen bg-sage-100">
+    <NavBar />
+    <main class=" max-w-4xl mx-auto-p6">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script>
+import NavBar from './NavBar.vue'
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="bg-[#CF5C36] text-[#050517] shadow-md">
+    <div class="max-w-7xl mx-auto px-4 py-3 items-center justify-between">
+      <h1 class="text-xl font-bold"> MyContacts</h1>
+
+      <ul class="flex space-x-6">
+        <li>
+          <RouterLink to:
+        </li>
+        <li>
+
+        </li>
+        <li>
+
+        </li>
+        <li>
+
+        </li>
+        <li>
+
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router';
+
+
+</script>
