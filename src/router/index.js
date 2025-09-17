02d6aba1c9e4d4c11fd02c53843fc51d00e7019e
toumaniaboubacarbamba <toumani.bamba@epitech.eu>
2025-09-17 12:02:15 +0000

    maj auth

--- src/router/index.js
+++ src/router/index.js
@@ -4,36 +4,51 @@ import LoginView from '@/views/LoginView.vue'
 import RegisterView from '@/views/RegisterView.vue'
 import ContactView from '@/views/ContactView.vue'
 import ContactDetails from '@/views/ContactDetails.vue'
+import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: LoginView,
-    },
-    {
-      path: '/register',
-      name: 'Register',
-      component: RegisterView,
-    },
-    {
-      path: '/contacts',
-      name: 'Contacts',
-      component: ContactView,
-    },
-    {
-      path: '/contacts/:id',
-      name: 'ContactDetails',
-      component: ContactDetails,
-    },
-  ],
+  routes,
 })
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+
+  if (to.meta.requiresAuth && !authStore.isAuth) {
+    next('/login')
+  } else if (to.meta.requireGuest && authStore.isAuth) {
+    next('/')
+  } else {
+    next()
+  }
+})
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: RegisterView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/contacts',
+    name: 'Contacts',
+    component: ContactView,
+  },
+  {
+    path: '/contacts/:id',
+    name: 'ContactDetails',
+    component: ContactDetails,
+  },
+]
 
 export default router
