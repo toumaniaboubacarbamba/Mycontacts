e8bc804f75f726cf704cd4bf4150c0910d41118a
toumaniaboubacarbamba <toumani.bamba@epitech.eu>
2025-09-17 11:53:57 +0000

    maj auth

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,14 +1,50 @@
-import { defineStore } from "pinia";
-import { computed, ref } from "vue";
+import { axios } from 'axios'
+import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
 
-export const useAuthStore = defineStore('auth', () =>{
+export const useAuthStore = defineStore('auth', () => {
   const user = ref(null)
   const token = ref(localStorage.getItem('token') || null)
   const isAuth = computed(() => !token.value)
 
-  const login = async( credentials) =>{
-    try{
-      const response = await axios.post()
+  const login = async (credentials) => {
+    try {
+      const response = await axios.post(
+        'https://api-contact.epi-bluelock.bj/api/users/login',
+        credentials,
+      )
+      user.value = response.data.user
+      token.value = response.data.token
+      isAuth.value = true
+      localStorage.setItem('token', token.value)
+      return response.data
+    } catch (error) {
+      throw error.response.data
     }
+  }
+
+  const register = async (userData) => {
+    try {
+      const response = await axios.post('https://api-contact.epi-bluelock.bj/api/users', userData)
+      return response.data
+    } catch (error) {
+      throw error.response.data
+    }
+  }
+
+  const logout = () => {
+    user.value = null
+    token.value = null
+    isAuth.value = false
+    localStorage.removeItem('token')
+  }
+
+  return {
+    user,
+    token,
+    isAuth,
+    login,
+    register,
+    logout,
   }
 })
